a17585f9097ba6b9501518cf14c8e36cd95e5729
Fausto Luna <kingfausto93@gmail.com>
2021-08-24 19:15:43 -0600

    Inicia el taller 3: calculamos promedio

--- README.md
+++ README.md
@@ -15,3 +15,10 @@
 2-Implementar nuestras fórmulas en JS
 3-Crear Funciones
 4-Integrar JS con HTML
+
+## Taller #3: Promedio, moda y mediana
+
+1-Defirnir las fórmulas.
+2-Implementar nuestras fórmulas en JS
+3-Crear Funciones
+4-Integrar JS con HTML
---
+++ promedio.js
@@ -0,0 +1,13 @@
+const lista1 = [100, 200, 300, 400];
+
+function calcular_promedio(lista) {
+  //   let suma_lista = 0;
+  //   for (let i = 0; i < lista.length; i++) {
+  //     suma_lista += lista[i];
+  //   }
+  let suma_lista = lista.reduce(function (valor_acumulado = 0, valor_actual) {
+    return valor_acumulado + valor_actual;
+  });
+  const promedio = suma_lista / lista.length;
+  return promedio;
+}
